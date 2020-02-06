77c07d64c2578bfb1791956d863fcf567879c78b
Jacob Smith <jacob.smith@unb.ca>
2020-02-05 21:50:43 -0400

    vusic now working as normal in monorepo

--- packages/split/vue.config.js
+++ packages/split/vue.config.js
@@ -2,6 +2,12 @@ module.exports = {
   chainWebpack: config => {
     config.resolve.symlinks(false);
   },
+  configureWebpack: {
+    externals: {
+      vue: "vue",
+      "@vue/composition-api": "@vue/composition-api"
+    }
+  },
   css: {
     extract: false
   }
